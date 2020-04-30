32cf3d4c05372954e67deaba3f820355ad269555
Jorge Juan Antolino Guil <jorgejnet@gmail.com>
2020-04-30 19:35:36 +0200

    Subida de foto y comentario a Firebase

--- app/src/main/java/com/jorgejag/cuidaportman/ProfileActivity.java
+++ app/src/main/java/com/jorgejag/cuidaportman/ProfileActivity.java
@@ -12,11 +12,11 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -37,19 +37,14 @@ import java.util.Date;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private static final int CAMERA_PERMIISSION_CODE = 101;
-    public static final int CAMERA_REQUEST_CODE = 102;
     private TextView textViewNombre;
     private TextView textViewCorreo;
 
     private Button btnSingOut;
-    private Button btnCapture;
-    private ImageView selectedImage;
+    private Button btnIncidencia;
 
     private FirebaseAuth auth;
     private DatabaseReference database;
-
-    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,27 +52,25 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         btnSingOut = findViewById(R.id.btnSingOut);
-        btnCapture = findViewById(R.id.btnCapture);
+        btnIncidencia = findViewById(R.id.btnIncidencia);
         textViewNombre = findViewById(R.id.textViewName);
-        selectedImage = findViewById(R.id.imgView);
 
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance().getReference();
 
-
         //textViewCorreo = findViewById(R.id.textViewEmail);
 
         //Accion de cada boton
-        btnCapture.setOnClickListener(new View.OnClickListener() {
+        btnIncidencia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Pedir permisos para usar la camera al usuario
-                askCameraPermissions();
-                //Toast.makeText(ProfileActivity.this, "El boton camara ha sido utilizado", Toast.LENGTH_SHORT).show();
+                //Envio hacia la activity upload
+                startActivity(new Intent(ProfileActivity.this, UploadActivity.class));
+                finish();
             }
         });
 
-
+        //Hacer logout
         btnSingOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,81 +84,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
 
-    //Solicitar permisos de camara
-    private void askCameraPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMIISSION_CODE);
-        } else {
-            dispatchTakePictureIntent();
-        }
-    }
-
-    //Llamada a metodo openCamara() si se han aceptado los permisos
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == CAMERA_PERMIISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                dispatchTakePictureIntent();
-            } else {
-                Toast.makeText(this, "Debe aceptar el uso de la camara", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                File f = new File(currentPhotoPath);
-                selectedImage.setImageURI(Uri.fromFile(f));
-            }
-
-        }
-    }
-
-    //Guardar la foto en la Galeria
-        private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.jorgejag.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-            }
-        }
-    }
-
-
-
+    //Trabaja con el usuario que ha iniciado sesion
     //Pedimos a la base de datos los datos del id que ha iniciado sesion
     private void getUserInfo() {
         String id = auth.getCurrentUser().getUid();
---
+++ app/src/main/java/com/jorgejag/cuidaportman/UploadActivity.java
@@ -0,0 +1,213 @@
+package com.jorgejag.cuidaportman;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UploadActivity extends AppCompatActivity {
+
+    private static final int CAMERA_PERMIISSION_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
+
+    private Button btnSend;
+    private ImageView selectedImage;
+    private ImageButton imgBtnCapture;
+    private EditText editTextComent;
+    private ProgressBar progressBar;
+    private Uri imageUri;
+
+    private StorageReference storageReference;
+    private DatabaseReference database;
+    private StorageTask uploadTask;
+
+    String currentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        btnSend = findViewById(R.id.btnEnviar);
+        imgBtnCapture = findViewById(R.id.imgButtonCapture);
+        selectedImage = findViewById(R.id.imageView);
+        editTextComent = findViewById(R.id.editTextComent);
+        progressBar = findViewById(R.id.progressBar);
+
+        storageReference = FirebaseStorage.getInstance().getReference("incidencias");
+        database = FirebaseDatabase.getInstance().getReference("incidencias");
+
+        //Accion de cada boton
+        imgBtnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Pedir permisos para usar la camera al usuario
+                askCameraPermissions();
+                //Toast.makeText(ProfileActivity.this, "El boton camara ha sido utilizado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (uploadTask != null && uploadTask.isInProgress()) {
+                    Toast.makeText(UploadActivity.this, "Subida en progreso", Toast.LENGTH_SHORT).show();
+                } else {
+                    uploadFile();
+                }
+            }
+        });
+    }
+
+    //Solicitar permisos de camara
+    private void askCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMIISSION_CODE);
+        } else {
+            dispatchTakePictureIntent();
+        }
+    }
+
+    //Llamada a metodo openCamara() si se han aceptado los permisos
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERMIISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent();
+            } else {
+                Toast.makeText(this, "Debe aceptar el uso de la camara", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                File f = new File(currentPhotoPath);
+                //selectedImage.setImageURI(Uri.fromFile(f));
+                imageUri = Uri.fromFile(f);
+                Picasso.get().load(imageUri).into(selectedImage);
+            }
+
+        }
+    }
+
+    //Crear el fichero de imagen
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  //* prefix *//*
+                ".jpg",         //* suffix *//*
+                storageDir      //* directory *//*
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.jorgejag.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
+    private void uploadFile() {
+        if (imageUri != null) {
+            //Damos un nombre unico a cada incidencia subida
+            StorageReference fileRerence = storageReference.child("incidencias" + System.currentTimeMillis() + ".jpg");
+
+            uploadTask = fileRerence.putFile(imageUri) .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //Retrasa el tiempo para la barra de progreso
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(0);
+                        }
+                    }, 500);
+                    Toast.makeText(UploadActivity.this, "Subida completada", Toast.LENGTH_SHORT).show();
+                    Upload upload = new Upload(editTextComent.getText().toString().trim() + " RESOLUCION PENDIENTE",
+                            taskSnapshot.getStorage().getDownloadUrl().toString());
+
+                    //Crea un id unico
+                    String uploadId = database.push().getKey();
+                    database.child(uploadId).setValue(upload);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(UploadActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());//Barra de progreso
+                    progressBar.setProgress((int) progress);
+                }
+            });
+
+        } else {
+            Toast.makeText(this, "No has realizado la foto", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
