c81426148c9023071cb583f3774d5b54b5421ac0
Jorge Juan Antolino Guil <jorgejnet@gmail.com>
2020-06-05 16:43:23 +0200

    Retoques finales.

--- app/src/main/java/com/jorgejag/cuidaportman/DetailsActivity.java
+++ app/src/main/java/com/jorgejag/cuidaportman/DetailsActivity.java
@@ -17,7 +17,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
